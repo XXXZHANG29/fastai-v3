7dde4c9d269692949eb7487102f5690b74228a21
XZNL <zhangxu19880@hotmail.com>
2019-06-20 21:03:29 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,13 +29,12 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `She is ${response["result"]}`;
     }
-    el("analyze-button").innerHTML = "Analyze";
+    el("analyze-button").innerHTML = "Check it out...";
   };
 
   var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
